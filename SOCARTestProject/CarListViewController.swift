13056f5867dcd63d5bec1f2ed9cdc47e8ca30c56
Jeonghoon Oh <ihoon9203@gmail.com>
2022-11-04 11:25:22 -0600

    title header for Favorite View Controller added

---
+++ SOCARTestProject/CarListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CarListViewController.swift
+//  SOCARTestProject
+//
+//  Created by Jeonghoon Oh on 11/4/22.
+//
+
+import UIKit
+
+class CarListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
